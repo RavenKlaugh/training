b15acad6246fed2d7ee45ea13e3084100ed44b94
jeclark5 <jeclark5@ncsu.edu>
2018-05-02 12:02:36 -0400

    Fixed test typos

--- src/myFirstArrayList/ArrayListTest.java
+++ src/myFirstArrayList/ArrayListTest.java
@@ -17,13 +17,13 @@ public class ArrayListTest {
 	@Test
 	public void testProgram() {
 		assertTrue(vin.add(1));
-		assertEquals(vin.get(1), 1);
+		assertEquals(1, vin.get(1));
 		for (int i = 2; i <= 10; i++) {
 			vin.add(i);
 		}
-		assertEquals(vin.get(9), 10);
+		assertEquals(10, vin.get(9));
 		assertTrue(vin.add(99));
-		assertEquals(vin.get(10), 99);
+		assertEquals(99, vin.get(10));
 	}
 
 }
